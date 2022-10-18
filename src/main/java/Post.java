81d5c932d13f9c8597aac8a9c6b736943b6f573b
trofimovpv <pashatrofimov@mail.ru>
2022-10-18 09:46:13 +0300

    First commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String phone;
+    public String passport;
+    public boolean subscription;
+
+    public FormDate birthday;
+}
